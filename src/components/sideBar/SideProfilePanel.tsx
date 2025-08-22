3c8fcad0707df6d6cdc2c114a7181151ebabb842
Suah-Cho <sacho0929@gmail.com>
2025-08-22 17:00:32 +0900

    feat: 사이드 프로필 패널 아이콘 추가

--- src/components/sideBar/SideProfilePanel.tsx
+++ src/components/sideBar/SideProfilePanel.tsx
@@ -1,11 +1,15 @@
 import { FiUser, FiSettings, FiLogOut } from "react-icons/fi";
+import { IoSettingsSharp } from "react-icons/io5";
+import { MdLogout } from "react-icons/md";
 
 export default function SideProfilePanel() {
     return (
         <div className="absolute bottom-14 left-2 w-48 bg-gray-700 text-white rounded shadow-lg p-2">
           <ul>
-            <li className="px-4 py-2 hover:bg-gray-600 cursor-pointer">설정</li>
-            <li className="px-4 py-2 hover:bg-gray-600 cursor-pointer">로그아웃</li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-600 cursor-pointer">
+              <IoSettingsSharp className="mr-2"/>설정</li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-600 cursor-pointer">
+              <MdLogout className="mr-2" />로그아웃</li>
           </ul>
         </div>
       );
